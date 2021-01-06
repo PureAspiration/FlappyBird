d45be2d8969dd1a6fa3b09c2aa8fb0965deb53ac
PureAspiration <PureAspirationOfficial@gmail.com>
2021-01-07 11:09:15 +1300

    Added Pipe Collision Detection

--- main.py
+++ main.py
@@ -24,6 +24,83 @@ red = (255, 0, 0)
 transparent = (0, 0, 0, 0)
 
 
+def gameoverScreen(pipes, birdPosX, birdPosY, floor, score):
+    background = pygame.image.load("assets/background-day.png").convert()
+    backgroundRect = background.get_rect()
+    backgroundRect.topleft = (0, 0)
+    screen.blit(background, backgroundRect)
+
+    for i in range(len(pipes)):
+        pipeTop = pygame.image.load("assets/pipe-green.png").convert()
+        pipeTop = pygame.transform.rotate(pipeTop, 180)
+        pipeRect = pipeTop.get_rect()
+        pipeRect.midbottom = (pipes[i][0], pipes[i][1] - 50)
+        screen.blit(pipeTop, pipeRect)
+
+        pipeBottom = pygame.image.load("assets/pipe-green.png").convert()
+        pipeRect = pipeBottom.get_rect()
+        pipeRect.midtop = (pipes[i][0], pipes[i][1] + 50)
+        screen.blit(pipeBottom, pipeRect)
+
+    bird = pygame.image.load("assets/yellowbird-upflap.png").convert()
+    birdRect = bird.get_rect()
+    birdRect.center = (birdPosX, birdPosY)
+    screen.blit(bird, birdRect)
+
+    base = pygame.image.load("assets/base.png").convert()
+    baseRect = base.get_rect()
+    baseRect.midright = (floor, 500)
+    screen.blit(base, baseRect)
+    base = pygame.image.load("assets/base.png").convert()
+    baseRect = base.get_rect()
+    baseRect.midleft = (floor, 500)
+    screen.blit(base, baseRect)
+
+    gameover = pygame.image.load("assets/gameover.png").convert_alpha()
+    gameoverRect = gameover.get_rect()
+    gameoverRect.center = (width / 2, 100)
+    screen.blit(gameover, gameoverRect)
+
+    scoreMenu = pygame.image.load("assets/scores.png").convert()
+    scoreMenu = pygame.transform.scale(scoreMenu, (200, 200))
+    scoreMenuRect = scoreMenu.get_rect()
+    scoreMenuRect.center = (width / 2, height / 2)
+    screen.blit(scoreMenu, scoreMenuRect)
+
+    button = pygame.image.load("assets/button-ok.png").convert()
+    button = pygame.transform.scale(button, (125, 41))
+    buttonRect = button.get_rect()
+    buttonRect.center = (width / 2, 400)
+    screen.blit(button, buttonRect)
+
+    global highScore
+    if score > highScore:
+        highScore = score
+        print("New High Score!")
+        scoreNew = pygame.image.load("assets/scores-new.png").convert()
+        scoreNew = pygame.transform.scale(scoreNew, (55, 26))
+        scoreNewRect = scoreNew.get_rect()
+        scoreNewRect.center = (115, 268)
+        screen.blit(scoreNew, scoreNewRect)
+        print("New High Score!")
+
+    for i in range(len(str(score))):
+        scoreDigit = str(score)[len(str(score)) - i - 1]
+        scoreNumber = pygame.image.load(f"assets/numbers/{scoreDigit}.png").convert_alpha()
+        scoreNumberRect = scoreNumber.get_rect()
+        scoreNumberRect.center = (195 - (i * 25), 230)
+        screen.blit(scoreNumber, scoreNumberRect)
+
+    for i in range(len(str(highScore))):
+        highScoreDigit = str(highScore)[len(str(highScore)) - i - 1]
+        scoreNumber = pygame.image.load(f"assets/numbers/{highScoreDigit}.png").convert_alpha()
+        scoreNumberRect = scoreNumber.get_rect()
+        scoreNumberRect.center = (195 - (i * 25), 305)
+        screen.blit(scoreNumber, scoreNumberRect)
+
+    pygame.display.update()
+
+
 def game():
     background = pygame.image.load("assets/background-day.png").convert()
     backgroundRect = background.get_rect()
@@ -35,7 +112,7 @@ def game():
     startScreenRect.center = (width / 2, height / 2)
     screen.blit(startScreen, startScreenRect)
 
-    birdPositionX, birdPositionY = 144, 304
+    birdPosX, birdPosY = 144, 304
 
     floor = 288
 
@@ -123,7 +200,7 @@ def game():
                     while birdJump:
                         clock.tick(30)
                         birdJumpPixels -= 10
-                        birdPositionY -= 10
+                        birdPosY -= 10
 
                         if birdJumpPixels <= -50:
                             birdJump = False
@@ -132,12 +209,12 @@ def game():
                     while not birdJump:
                         clock.tick(30)
                         birdFallPixels = birdFallPixels + gravity
-                        birdPositionY += birdFallPixels
+                        birdPosY += birdFallPixels
                         break
 
                     bird = pygame.image.load("assets/yellowbird-upflap.png").convert()
                     birdRect = bird.get_rect()
-                    birdRect.center = (birdPositionX, birdPositionY)
+                    birdRect.center = (birdPosX, birdPosY)
                     screen.blit(bird, birdRect)
 
                     base = pygame.image.load("assets/base.png").convert()
@@ -196,87 +273,36 @@ def game():
 
                     pygame.display.update()
 
-                    if birdPositionY <= -50:
+                    if birdPosY <= -50:
                         print("Out Of Bounds")
-                        birdPositionY = -49
+                        birdPosY = -45
 
-                    if birdPositionY >= 430:
+                    for i in range(len(pipes)):
+                        if pipes[i][0] - 40 <= birdPosX <= pipes[i][0] + 40:
+                            if (birdPosY <= pipes[i][1] - 40) or (birdPosY >= pipes[i][1] + 40):
+                                print("Game over")
+
+                                gameoverScreen(pipes, birdPosX, birdPosY, floor, score)
+
+                                while True:
+                                    for event in pygame.event.get():
+
+                                        if event.type == pygame.QUIT:
+                                            pygame.quit()
+                                            exit()
+
+                                        elif event.type == pygame.MOUSEBUTTONDOWN:
+                                            mouse = pygame.mouse.get_pos()
+                                            print(f"{mouse[0]} {mouse[1]}")
+
+                                            if 82 < mouse[0] < 206 and 380 < mouse[1] < 420:
+                                                print("Restarting")
+                                                game()
+
+                    if birdPosY >= 430:
                         print("Game over")
 
-                        background = pygame.image.load("assets/background-day.png").convert()
-                        backgroundRect = background.get_rect()
-                        backgroundRect.topleft = (0, 0)
-                        screen.blit(background, backgroundRect)
-
-                        for i in range(len(pipes)):
-                            pipeTop = pygame.image.load("assets/pipe-green.png").convert()
-                            pipeTop = pygame.transform.rotate(pipeTop, 180)
-                            pipeRect = pipeTop.get_rect()
-                            pipeRect.midbottom = (pipes[i][0], pipes[i][1] - 50)
-                            screen.blit(pipeTop, pipeRect)
-
-                            pipeBottom = pygame.image.load("assets/pipe-green.png").convert()
-                            pipeRect = pipeBottom.get_rect()
-                            pipeRect.midtop = (pipes[i][0], pipes[i][1] + 50)
-                            screen.blit(pipeBottom, pipeRect)
-
-                        bird = pygame.image.load("assets/yellowbird-upflap.png").convert()
-                        birdRect = bird.get_rect()
-                        birdRect.center = (birdPositionX, birdPositionY)
-                        screen.blit(bird, birdRect)
-
-                        base = pygame.image.load("assets/base.png").convert()
-                        baseRect = base.get_rect()
-                        baseRect.midright = (floor, 500)
-                        screen.blit(base, baseRect)
-                        base = pygame.image.load("assets/base.png").convert()
-                        baseRect = base.get_rect()
-                        baseRect.midleft = (floor, 500)
-                        screen.blit(base, baseRect)
-
-                        gameover = pygame.image.load("assets/gameover.png").convert_alpha()
-                        gameoverRect = gameover.get_rect()
-                        gameoverRect.center = (width / 2, 100)
-                        screen.blit(gameover, gameoverRect)
-
-                        scoreMenu = pygame.image.load("assets/scores.png").convert()
-                        scoreMenu = pygame.transform.scale(scoreMenu, (200, 200))
-                        scoreMenuRect = scoreMenu.get_rect()
-                        scoreMenuRect.center = (width / 2, height / 2)
-                        screen.blit(scoreMenu, scoreMenuRect)
-
-                        button = pygame.image.load("assets/button-ok.png").convert()
-                        button = pygame.transform.scale(button, (125, 41))
-                        buttonRect = button.get_rect()
-                        buttonRect.center = (width / 2, 400)
-                        screen.blit(button, buttonRect)
-
-                        global highScore
-                        if score > highScore:
-                            highScore = score
-                            print("New High Score!")
-                            scoreNew = pygame.image.load("assets/scores-new.png").convert()
-                            scoreNew = pygame.transform.scale(scoreNew, (55, 26))
-                            scoreNewRect = scoreNew.get_rect()
-                            scoreNewRect.center = (115, 268)
-                            screen.blit(scoreNew, scoreNewRect)
-                            print("New High Score!")
-
-                        for i in range(len(str(score))):
-                            scoreDigit = str(score)[len(str(score)) - i - 1]
-                            scoreNumber = pygame.image.load(f"assets/numbers/{scoreDigit}.png").convert_alpha()
-                            scoreNumberRect = scoreNumber.get_rect()
-                            scoreNumberRect.center = (195 - (i * 25), 230)
-                            screen.blit(scoreNumber, scoreNumberRect)
-
-                        for i in range(len(str(highScore))):
-                            highScoreDigit = str(highScore)[len(str(highScore)) - i - 1]
-                            scoreNumber = pygame.image.load(f"assets/numbers/{highScoreDigit}.png").convert_alpha()
-                            scoreNumberRect = scoreNumber.get_rect()
-                            scoreNumberRect.center = (195 - (i * 25), 305)
-                            screen.blit(scoreNumber, scoreNumberRect)
-
-                        pygame.display.update()
+                        gameoverScreen(pipes, birdPosX, birdPosY, floor, score)
 
                         while True:
                             for event in pygame.event.get():
